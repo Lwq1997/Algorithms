bd30edeebde1b4c3043bcbcfea673ba0ac6420f7
Lwq <liwanqi@etouch.cn>
2018-08-09 09:49:28 +0800

    接触图类算法，稠密图用领接矩阵表示

---
+++ src/main/Part1/Chapter7GraphBasic/DenseGraph.java
@@ -0,0 +1,60 @@
+package main.Part1.Chapter7GraphBasic;
+
+/**
+ * @author Lwq
+ * @create 2018-08-09 9:34
+ * @desc 稠密图，邻接矩阵
+ **/
+public class DenseGraph {
+//    图的顶点数
+    private int vertices;
+
+//    图的边数
+    private int edges;
+
+//    有向图还是无向图
+    private boolean directed;
+
+//    领接矩阵
+    private boolean[][]  adj;
+
+    DenseGraph(int vertices,boolean directed){
+        this.vertices = vertices;
+        this.directed = directed;
+        this.edges = 0;
+        adj = new boolean[vertices][vertices];
+    }
+
+//    返回顶点数
+    public int V(){
+        return vertices;
+    }
+
+//    返回边数
+    private int E(){
+        return edges;
+    }
+
+//    添加边(在v和w之间建立一条边)
+    public void addEdge(int v,int w){
+        assert (v>=0&&v<vertices);
+        assert (w>=0&&w<vertices);
+
+        if(hasEdge(v,w)){
+            return;
+        }
+
+        adj[v][w] = true;
+        if(!directed){
+            //无向图实际是双向图
+            adj[w][v] = true;
+        }
+        edges++;
+    }
+
+    private boolean hasEdge(int v, int w) {
+        assert (v>=0&&v<vertices);
+        assert (w>=0&&w<vertices);
+        return adj[v][w];
+    }
+}
