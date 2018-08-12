38bbba0021c5afd91fc2c420434919eb190007eb
Lwq <943904454@qq.com>
2018-08-12 23:13:22 +0800

    实现一个数组

---
+++ src/main/Part2/Array.java
@@ -0,0 +1,150 @@
+package main.Part2;
+
+/**
+ * @Author: Lwq
+ * @Date: 2018/8/12 21:21
+ * @Version 1.0
+ * @Describe
+ */
+public class Array<E> {
+    private E[] data;
+    private int size;
+
+    private Array(int capacity){
+        data = (E[]) new Object[capacity];
+        size = 0;
+    }
+
+    public Array(){
+        this(10);
+    }
+
+    public int getCapacity(){
+        return data.length;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size==0;
+    }
+
+    public void add(int index,E e){
+        if(size==data.length){
+            resize(2*data.length);
+        }
+
+        if(index<0||index>size){
+            throw new IllegalArgumentException("Add failed. Require index >= 0 and index <= size.");
+        }
+
+        for(int i=size-1;i>=index;i--){
+            data[i+1] = data[i];
+        }
+        data[index] = e;
+        size++;
+    }
+
+    //数组大小变为newCapacity
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity];
+        for(int i = 0; i < size;i++){
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
+
+    public void addFirst(E e){
+        add(0,e);
+    }
+
+    public void getLast(E e){
+        add(size,e);
+    }
+
+    public E get(int index){
+        if(index < 0 || index >= size)
+            throw new IllegalArgumentException("Get failed. Index is illegal.");
+        return data[index];
+    }
+
+    public E getFirst(){
+        return data[0];
+    }
+
+    public E getLast(){
+        return data[size-1];
+    }
+
+    //将索引index处的元素替换为e
+    public void set(int index,E e){
+        if(index < 0 || index >= size)
+            throw new IllegalArgumentException("Set failed. Index is illegal.");
+        data[index] = e;
+    }
+
+    public boolean contains(E e){
+        for(int i = 0; i <size;i++){
+            if(data[i].equals(e)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int find(E e){
+        for(int i = 0; i <size;i++){
+            if(data[i].equals(e)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public E remove(int index){
+        if(index < 0 || index >= size)
+            throw new IllegalArgumentException("Remove failed. Index is illegal.");
+        E ret = data[index];
+        for(int i = index+1;i <size;i++){
+            data[i-1] = data[i];
+        }
+        size--;
+        data[size] = null;
+        if(size==data.length/4&&data.length/2!=0){
+            resize(data.length/2);
+        }
+        return ret;
+    }
+
+    public E removeFirst(){
+        return remove(0);
+    }
+
+    public E removeLast(){
+        return remove(size-1);
+    }
+
+    public void removeElement(E e){
+        int index = find(e);
+        if(index!=-1){
+            remove(index);
+        }
+    }
+
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array: size = %d , capacity = %d\n", size, data.length));
+        res.append('[');
+        for(int i = 0; i < size; i++){
+            res.append(data[i]);
+            if (i != size-1){
+                res.append(", ");
+            }
+        }
+        res.append(']');
+        return res.toString();
+    }
+
+}
